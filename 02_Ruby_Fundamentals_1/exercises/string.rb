20808f0f813e25698d93a0dad452c1dbaaee491f
tjsz <tjsz@icloud.com>
2015-08-03 20:21:58 -0700

    string methods

---
+++ 02_Ruby_Fundamentals_1/exercises/string.rb
@@ -0,0 +1,16 @@
+greeting = "hey Girl, Hey!"
+name = "Theresa"
+bewd_class = "back end web development"
+
+puts name << "greeting"
+puts name.strip
+
+color = "green"
+name.prepend("My name is ")
+color.prepend("the best color is ")
+puts name
+puts color
+table  = "the table is tall ".strip
+puts table
+
+name.chars
