69ad5f97b608b6ba8ad41a06df62f3abf1846d8f
Kisha Richardson <kisha@spunklabs.com>
2015-08-16 12:39:52 -0700

    updates to get_it

--- 05_Ruby_Fundamentals/exercises/get_it.rb
+++ 05_Ruby_Fundamentals/exercises/get_it.rb
@@ -1,6 +1,5 @@
 #1 - Pull the json from the reddit API via http://www.reddit.com/.json
   # - http://mashable.com/stories.json (homework)
-  # - http://digg.com/api/news/popular.json (Homework)
 #2 - Parse it using the JSON library
 #3 - Find the stories based on techniques used in the code_along (max of 25 provided)
 #4 - Create a new story hash out of each story with the following keys :title, :upvotes and :category
@@ -16,9 +15,33 @@ require 'pry'
 require 'json'
 
 def connect_to_api(url)
-  get_data = RestClient.get(url)
-  JSON.parse(get_data)
+  response = RestClient.get(url)
+  JSON.parse(response)
 end
 
-reddit_url = "http://www.reddit.com/.json"
-connect_to_api(reddit_url)
+def find_stories(response)
+  stories = response["data"]["children"]
+  puts "*******Reddit has blessed us with #{stories.count} stories *******"
+  return stories
+end
+
+def print_stories(stories)
+  stories.each do |story|
+    create_story_hash(story["data"])
+  end
+end
+
+def create_story_hash(story)
+  {category: story["subreddit"], title: story["title"], upvotes: story["ups"]}
+end
+
+reddit_url ="http://www.reddit.com/.json"
+reddit_json_response = connect_to_api(reddit_url)
+stories = find_stories(reddit_json_response)
+print_stories(stories)
+
+
+
+
+
+#####
