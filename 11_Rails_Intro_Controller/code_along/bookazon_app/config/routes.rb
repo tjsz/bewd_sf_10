92cfa6b10a32ede3e4c0f7961856ef78b84fc90d
Kisha Richardson <kisha@spunklabs.com>
2015-09-08 15:50:50 -0700

    rearrange file structure

--- 11_Rails_Intro_Controller/code_along/bookazon_app/config/routes.rb
+++ 11_Rails_Intro_Controller/code_along/bookazon_app/config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  get 'books/new'
 
-  get 'books/index'
-
-  get 'books/show'
-
-  get 'books/edit'
-
+  resources :books
   #this sets your default_page
   root 'static_pages#home'
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
