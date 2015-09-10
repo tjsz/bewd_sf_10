8e6dfa010403e59f1113927c8a9ed173a06d034e
Kisha Richardson <kisha@spunklabs.com>
2015-09-09 21:18:14 -0700

    updates to edit/update actions && new/show view

--- 12_Rails_Intro_Controller_Continued/code_along/bookazon_app/app/controllers/books_controller.rb
+++ 12_Rails_Intro_Controller_Continued/code_along/bookazon_app/app/controllers/books_controller.rb
@@ -22,9 +22,16 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = get_book
   end
 
   def update
+    @book = get_book
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
