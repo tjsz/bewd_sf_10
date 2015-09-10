55f86b28b61f123ebeaff0ad2392c3c90e9a153a
Kisha Richardson <kisha@spunklabs.com>
2015-09-09 21:06:04 -0700

    updates to books_controller & view

--- 12_Rails_Intro_Controller_Continued/code_along/bookazon_app/app/controllers/books_controller.rb
+++ 12_Rails_Intro_Controller_Continued/code_along/bookazon_app/app/controllers/books_controller.rb
@@ -1,17 +1,24 @@
 class BooksController < ApplicationController
 
-  #READ: this will retrieve all books thru active_record
   def index
     @books = Book.all
   end
 
   def show
+    @book = get_book
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -26,5 +33,10 @@ class BooksController < ApplicationController
   private
 
   def book_params
+    params.require(:book).permit(:title, :year_published, :author, :available, :genre, :image_url)
+  end
+
+  def get_book
+    Book.find(params[:id])
   end
 end
