1c9a35e6dcfa544801869a45ffdc227ef4779f43
Kisha Richardson <kisha@spunklabs.com>
2015-09-16 19:38:36 -0700

    add bootstrap & simple_form to stack

--- 14_Rails_Intro_Front_End/guidance/my_music_app/config/routes.rb
+++ 14_Rails_Intro_Front_End/guidance/my_music_app/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  #establish a custom route
+  get 'about', to: "static_pages#about", as: "about"
+
+
+  get 'test', to: "static_pages#test", as: "test"
+ #establish a root_path
+  root 'static_pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
